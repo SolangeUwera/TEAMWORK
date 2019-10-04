51d9255f33b69a68c6737a91ddb1cce836463846
SolangeUwera <usolange13@gmail.com>
2019-10-04 10:21:27 +0200

    bugging UI [finish#168943979]

--- server/models/data.js
+++ server/models/data.js
@@ -1,65 +1,72 @@
 const data = {
-    users: [{
-        id:1,
-        firstName: 'admin',
-        lastName: 'bar',
-        email: 'admin@gmail.com',
-        password: '123456six',
-        Gender:'F',
-	    JobRole:'IT',
-	    Department:'c.s',
-	    Adress:'Musha'
-    },{
-        id:2,
-        firstName: 'SOLANGE',
-        lastName:'UWERA',
-        email: 'usolange13@gmail.com',
-        password: '01234five',
-        Gender:'F',
-	    JobRole:'IT',
-	    Department:'c.s',
-	    Adress:'Musha'
+  users: [
+    {
+      id: 1,
+      firstName: "admin",
+      lastName: "bar",
+      email: "admin@gmail.com",
+      password: "123456six",
+      Gender: "F",
+      JobRole: "IT",
+      Department: "c.s",
+      Adress: "Musha"
+    },
+    {
+      id: 2,
+      firstName: "SOLANGE",
+      lastName: "UWERA",
+      email: "usolange13@gmail.com",
+      password: "01234five",
+      Gender: "F",
+      JobRole: "IT",
+      Department: "c.s",
+      Adress: "Musha"
+    },
+    {
+      id: 3,
+      firstName: "SOLANGE",
+      lastName: "UWERA",
+      email: "uwera@gmail.com",
+      password: "01234five",
+      Gender: "F",
+      JobRole: "IT",
+      Department: "c.s",
+      Adress: "Musha"
+    }
+  ],
 
-}, {
-    id:3,
-firstName: 'SOLANGE',
-lastName:'UWERA',
-email: 'usolange13@gmail.com',
-password: '01234five',
-Gender:'F',
-JobRole:'IT',
-Department:'c.s',
-Adress:'Musha'}],
-
- articles :[{
-    articleId:1,
-    createdon:'2019-09-19T14:14:57.985Z',
-    title:'businessmodel',
-    article:'ttttdddddddddd',
-    authorId:1
-},
-{
- articleId:2,
- createdon:'2019-09-19T14:11:57.985Z',
-title:'pppp',
-article:'ttttdddddddddd',
-authorId:2
- 
-},
-{
-    articleId:3,
-    createdon:'2019-09-19T14:14:57.985Z',
-    title:'businessmodel',
-    article:'ttttdddddddddd',
-    authorId:2
-}],
-comments : [{
-    commentId:1,
-    createdon:'01/01/2000',
-    articletitle:'bos',
-    article:'hhhhhhhhhhh',
-    comment:'waoh',
-    authorId:2
-}]
+  articles: [
+    {
+      articleId: 1,
+      createdon: "2019-09-19T14:14:57.985Z",
+      title: "businessmodel",
+      article: "ttttdddddddddd",
+      authorId: 1
+    },
+    {
+      articleId: 2,
+      createdon: "2019-09-19T14:11:57.985Z",
+      title: "pppp",
+      article: "ttttdddddddddd",
+      authorId: 2
+    },
+    {
+      articleId: 3,
+      createdon: "2019-09-19T14:14:57.985Z",
+      title: "businessmodel",
+      article: "ttttdddddddddd",
+      authorId: 2
+    }
+  ],
+  comments: [
+    {
+      commentId: 1,
+      createdon: "01/01/2000",
+      articletitle: "bos",
+      article: "hhhhhhhhhhh",
+      comment: "waoh",
+      authorId: 2
+    }
+  ]
 };
 export default data;
